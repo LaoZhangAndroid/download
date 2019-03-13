8a767faebd96ec08c5eb3cd939313bf8dc36788c
laozhang <826231415@qq.com>
2019-03-13 17:29:02 +0800

    commit

--- app/src/main/java/com/laozhang/download/MainActivity.java
+++ app/src/main/java/com/laozhang/download/MainActivity.java
@@ -1,13 +1,40 @@
 package com.laozhang.download;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int CODE_PHOTO_PICK = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView photosView = findViewById(R.id.photosView);
+        photosView.setLayoutManager(new GridLayoutManager(this, 2));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==CODE_PHOTO_PICK && resultCode==RESULT_OK && data!=null){
+            Uri uri = data.getData();
+
+        }
+    }
+
+    public void addPhoto(View view){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"Browser Image..."), CODE_PHOTO_PICK);
     }
 }
